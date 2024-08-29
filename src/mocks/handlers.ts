064afa6b123d265bca937e2786d085b0ea27a75c
Haythem Sahbani <sahbani.haythem@gmail.com>
2024-08-29 14:40:57 +0200

    Add passthrough example

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,21 +1,11 @@
-import { bypass, http, HttpResponse } from 'msw';
+import { bypass, http, HttpResponse, passthrough } from 'msw';
 
 export const handlers = [
-  http.get('https://pokeapi.co/api/v2/pokemon/0', () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: 0,
-      name: 'msw',
-    });
-  }),
-
-  http.get('https://pokeapi.co/api/v2/pokemon/:id', async ({ request }) => {
-    const pokemon = await fetch(bypass(request)).then((response) =>
-      response.json()
-    )
- 
-    const { id, name, types } = pokemon;
-
-    return HttpResponse.json({ id, name, types, legendary: name === 'mew'  })
-  }),
+  http.get('https://pokeapi.co/api/v2/pokemon/:id', ({ params }) => {
+    if (params['id'] === '0') {
+        return HttpResponse.json({ mocked: true, id:0, name: 'msw' })
+    }
+    
+    return passthrough();
+  })
 ];

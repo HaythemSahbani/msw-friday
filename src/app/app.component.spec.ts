bb03b6a8f8cbe9cd203309d51ba9321e22f4ab3e
Haythem Sahbani <sahbani.haythem@gmail.com>
2024-08-29 23:15:16 +0200

    handle api errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { server } from '../mocks/serve';
+import { HttpResponse, http } from 'msw';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -27,5 +29,16 @@ describe('AppComponent', () => {
     const response = await firstValueFrom(component.pokemon$);
 
     expect(response).toEqual({id: 0, name: 'msw'});
+  });
+
+  it('should handle api errors', async () => {  
+    server.use(http.get('https://pokeapi.co/api/v2/pokemon/:id', () => {
+      return new HttpResponse(null, { status: 500 })
+    }));
+    component.fetchPokemon('0');
+
+    const response = await firstValueFrom(component.pokemon$);
+
+    expect(response.status).toBe(500);
   });
 });
